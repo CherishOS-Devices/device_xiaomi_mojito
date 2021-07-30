2287d1f1a9e2abb33345f5b06dcc94290539f2b7
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-30 19:18:09 +0600

    sm6150-common: rootdir: Import device fstab from stock
    Co-authored-by: Albert I <kras@raphielgang.org>
    Change-Id: Ibc910f2349f09bb7af2dbb1d87b8acaeb749b0dd

--- sm6150.mk
+++ sm6150.mk
@@ -301,6 +301,7 @@ PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
     init.qcom.sh \
     fstab.zram \
+    fstab.qcom \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
