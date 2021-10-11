fb463a71575b39dc2087e7e96bebfa1b3865e0df
Henrique Pereira <hlcpereira@pixelexperience.org>
2021-10-11 15:47:41 -0300

    sm6150-common: Remove non-existent camera packages
    * Those not even on R exist
    * Partially reverts 27538359d4ad64e14b0d9ab74030f76e592c3e48
    
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>
    Change-Id: Id33c4a6a4eae182cc9a5cd94bb7693978aab6921

--- sm6150.mk
+++ sm6150.mk
@@ -113,9 +113,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
-    vendor.qti.hardware.camera.device@1.0.vendor \
-    vendor.qti.hardware.camera.device@2.0.vendor \
-    vendor.qti.hardware.camera.device@3.5.vendor
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
