6299c6ddf903f0038f661d39ec43d9888033ba34
adi8900 <adrianszymanski242@gmail.com>
2021-12-30 21:48:37 +0100

    sm6150-common: Drop android.hardware.bluetooth@1.0.vendor
    * android.hardware.bluetooth@1.1.vendor builds 1.0 as well
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>
    Signed-off-by: TejasKamat <kamattejas23@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -118,7 +118,6 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
-    android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio@2.1-impl \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
