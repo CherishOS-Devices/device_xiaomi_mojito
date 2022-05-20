cb00df2a74c325c875b3cf5b7b85175e0a57d1a5
TejasKamat <kamattejas23@gmail.com>
2022-05-20 22:05:21 +0530

    mojito: evolution-ify
    Signed-off-by: TejasKamat <kamattejas23@gmail.com>

---
+++ evolution_mojito.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2021 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from mojito device
+$(call inherit-product, device/xiaomi/mojito/device.mk)
+
+# Inherit some Evolution X stuffs
+TARGET_SUPPORTS_QUICK_TAP := true
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := evolution_mojito
+PRODUCT_DEVICE := mojito
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 10
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
