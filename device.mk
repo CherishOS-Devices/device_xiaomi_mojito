2c80fdfb3f9b25ce2129e60e92e0942b257b7333
Bruno Martins <bgcngm@gmail.com>
2022-10-06 11:27:44 +0600

    mojito: Drop bluetooth component-override
    This reverts commit 58b34eb7d7898efef4452c37be78cb65d2552ca6.
    
    Change-Id: I393d36e62749f4ce948995c5870debde943f108d
    Signed-off-by: TejasKamat <kamattejas23@gmail.com>

--- device.mk
+++ device.mk
@@ -121,9 +121,6 @@ PRODUCT_PACKAGES += \
     libldacBT_bco
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
-PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
