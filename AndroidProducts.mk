1d861a9c1f94142798e5f3aa9957055225986a0f
BishalDas <dabishal3456@gmail.com>
2023-04-20 21:43:18 +0530

    mojito: Let's go official
    Signed-off-by: BishalDas <dabishal3456@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_mojito.mk
+    $(LOCAL_DIR)/cherish_mojito.mk
--- cherish_mojito.mk
+++ cherish_mojito.mk
@@ -11,15 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some Evolution X stuffs
+# Inherit some CherishOS stuffs
 TARGET_SUPPORTS_QUICK_TAP := true
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := evolution_mojito
+PRODUCT_NAME := cherish_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
@@ -33,3 +33,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="redfin-user 12 SP1A.211105.003 7757856 release-keys"
 
 BUILD_FINGERPRINT :="google/redfin/redfin:12/SP1A.211105.003/7757856:user/release-keys"
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=BishalDas
+CHERISH_BUILD_TYPE := OFFICIAL
+
